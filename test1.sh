8c285d373ee0a13f6f9ecdc6ce91b4c4b2332750
hackadaynow <qinglin.jiang@gmail.com>
2026-06-23 21:12:26 -0700

    Update test1.sh for version 2 output

--- test1.sh
+++ test1.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-  # continuous-deploy check
+ #!/bin/bash
   TS=$(date -u +%FT%TZ)
-  echo "test1 v1 OK $TS"                         
-  echo "test1 v1 OK $TS" >> "$HOME/test1_out.txt" # 
+  echo "test1 v2 OK $TS"
+  echo "test1 v2 OK $TS" >> "$HOME/test1_out.txt"
+  echo "diag: cfg=$(env | wc -l) cwd=$(pwd) who=$(whoami)" >> "$HOME/test1_out.txt"
